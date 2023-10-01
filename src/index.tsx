0476c7f72335f4dd79b3777c3adbf28ce1bc099b
zeriong <jaeryong95@gmail.com>
2023-10-01 17:20:47 +0900

    레이아웃 폼 구성

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/layout/Aside.tsx
+++ src/layout/Aside.tsx
@@ -1,12 +1,39 @@
+import {useRef} from 'react';
+
+interface IPatList {
+    name: string;
+    texture: any;
+    lv: number;
+}
+
 export const Aside = () => {
+    const patListRef = useRef<IPatList[]>([
+        {
+            name: '시바',
+            texture: '3d 모델',
+            lv: 10,
+        },
+        {
+            name: '야옹이',
+            texture: '3d 모델',
+            lv: 10,
+        },
+    ]);
+
     return (
-        <aside className='relative left-0 bg-black w-[360px]'>
-            <div>
-                시바
-            </div>
-            <div>
-                페르시안
-            </div>
+        <aside className='flex flex-col gap-[16px] relative left-0 bg-gray-200 w-[360px] h-full p-[20px]'>
+            {patListRef.current && patListRef.current.map((pat, idx) => (
+                    <div key={idx} className='flex bg-red-500 h-[112px] rounded-2xl p-[16px] hover:scale-105 ease-in-out duration-200'>
+                        <div className='rounded-xl bg-white w-[80px] h-[80px]'>
+                            {pat.texture}
+                        </div>
+                        <div className='pl-[12px]'>
+                            <h1>{pat.lv}</h1>
+                            <h1>{pat.name}</h1>
+                        </div>
+
+                    </div>
+            ))}
         </aside>
     )
 }
